d020354fb123f8090ce1d6923a2c43b2377cf702
Daniella Zuccolotto <danielaz_9@hotmail.com>
2022-12-06 17:42:25 -0300

    feat: Implementei a funçao filter_by_salary_range

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -85,4 +85,11 @@ def filter_by_salary_range(
     list
         Jobs whose salary range contains `salary`
     """
-    raise NotImplementedError
+    list_jobs = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                list_jobs.append(job)
+        except ValueError as error:
+            print(error)
+    return list_jobs
